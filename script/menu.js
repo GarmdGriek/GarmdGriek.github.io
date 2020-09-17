5c1cf3d18ef14acee30fcc812da9f64dbf4eb39a
GarmdGriek <68013815+GarmdGriek@users.noreply.github.com>
2020-09-17 13:25:53 +0200

    fix

--- script/menu.js
+++ script/menu.js
@@ -25,7 +25,7 @@ function menu() {
         var menuJuliLink=["<a href=/blogpost/20200701-creating-the-blog.html>", "<a href=/blogpost/20200702-make-the-navigation-easy.html>","<a href=/blogpost/20200703-making-decisions.html>","<a href=/blogpost/20200704-weekend.html>","<a href=/blogpost/20200704-menu-work.html>","<a href=/blogpost/20200706-redesign.html>","<a href=/blogpost/20200708-javascript-simplification.html>","<a href=/blogpost/20200716-few-days.html>","<a href=/blogpost/20200717-vacation.html>","<a href=/blogpost/20200725-home-again.html>"]; //blog titles and links
         var menuAugust=["13. Back to work"]; //blog titles and links
         var menuAugustLink=["<a href=/blogpost/20200813-back-to-work.html>"]; //blog titles and links
-        var menuSeptember=["2. A month and a Tesla","17. Moving to Github"]; //blog titles and links
+        var menuSeptember=["2. A month and a Tesla", "17. Moving to Github"]; //blog titles and links
         var menuSeptemberLink=["<a href=/blogpost/20200902-a-month-and-a-tesla.html>","<a href=/blogpost/20200917-moving-to-github.html"]; //blog titles and links
         var menuPresenceText =["Twitter", "LinkedIn","Facebook","StackOverflow","FreeCodeCamp.org"]; //Social presence titles and links
         var menuPresenceLink=["<a href=https://twitter.com/NicolaiImset target=\"_blank\">", "<a href=https://www.linkedin.com/in/nicolaiimset/ target=\"_blank\">","<a href=https://www.facebook.com/nicolai.imset target=\"_blank\">","<a href=https://stackoverflow.com/users/13789221/nicolai-imset target=\"_blank\">","<a href=https://www.freecodecamp.org/fccd7f7b8c8-3869-4fe1-b908-d364e64f59e3 target=\"_blank\">"]; //Social presence titles and links
